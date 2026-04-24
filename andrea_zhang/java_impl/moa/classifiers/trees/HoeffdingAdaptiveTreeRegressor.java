c7c9c75035df06fb6acb14d842c073daaa13977e
Andrea Zhang <andrea02polimi@gmail.com>
2026-04-24 23:12:55 +0200

    added some of base parameters of River's HoeffdingTree class

--- andrea_zhang/java_impl/moa/classifiers/trees/HoeffdingAdaptiveTreeRegressor.java
+++ andrea_zhang/java_impl/moa/classifiers/trees/HoeffdingAdaptiveTreeRegressor.java
@@ -4,5 +4,15 @@ import moa.classifiers.AbstractClassifier;
 import moa.classifiers.Regressor;
 
 public class HoeffdingAdaptiveTreeRegressor extends AbstractClassifier implements Regressor {
-    
+    private static final long serialVersionUID = 1L;
+
+    // all parameters in River's HoeffdingTree
+    protected Node root;
+    protected double trainWeightSeen = 0; 
+    protected int nActiveLeaves = 0; 
+    protected int nInactiveLeaves = 0; 
+    protected boolean growthAllowed = true; 
+    private double sizeEstimateOverhead = 1.0; // fattore di correzione usato per stimare meglio quanta memoria occupa davvero l’albero. Parte da 1.0 perché all’inizio non sa ancora quanto la stima sia imprecisa, quindi assume temporaneamente che la stima sia perfetta.
+    private   double  activeLeafSizeEstimate   = 0.0;
+    private   double  inactiveLeafSizeEstimate = 0.0;
 }
